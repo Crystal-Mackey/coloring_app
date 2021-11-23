644bb117f73f114eb21aebac440a1da8fdabe438
Crystal Mackey <moonrunner575@gmail.com>
2021-11-23 15:36:40 -0500

    added how to video to homepage

--- src/App.js
+++ src/App.js
@@ -3,38 +3,32 @@ import "./App.css";
 import Home from "./components/home/Home";
 import Form from "./components/contact/Contact";
 import About from "./components/about/About";
-import {Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/navigation/NavBar";
-import FlowerPage from "./components/flower/Flowerpage"
+import FlowerPage from "./components/flower/Flowerpage";
 import FiretruckPage from "./components/firetruck/Firetruckpage";
 
 const App = () => {
-  // const [fillColors, setFillColors] = useState(Array(22).fill("white"));
-  // const [currentColor, setCurrentColor] = useState("blue");
-
-  // const onFillColor = (i) => {
-  //   let newFillColors = fillColors.slice(0);
-  //   newFillColors[i] = currentColor;
-  //   setFillColors(newFillColors);
-  // };
-
-  
   return (
     <div className="App">
       <div className="page">
-     <div className="Navbar">
-      <NavBar/>
+        <section>
+          <header>
+            <div className="Navbar">
+              <NavBar />
+            </div>
+          </header>
+        </section>
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Form />} />
+          <Route exact path="/flower" element={<FlowerPage />} />
+          <Route exact path="/firetruck" element={<FiretruckPage />} />
+        </Routes>
       </div>
-      
-    <Routes>
-     <Route exact path="/" element={<Home/>} />
-     <Route exact path="/about" element={<About/>} />
-     <Route exact path="/contact" element={<Form/>} />
-     <Route exact path="/flower" element={<FlowerPage/>} />
-     <Route exact path="/firetruck" element={<FiretruckPage/>} />
-     </Routes>
-    
-      </div></div>
+    </div>
   );
 };
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import video from "../video/ColoringFlower.mp4"
 
 function Home() {
     return(
+        <>
         <div>
             <h1>Welcome to my Website</h1>
 
             <p>This chance to color digitally is partially inspired by my little man.</p>
             <p>Navigate to one of the other links for a chance to choose from a variety of colors to make a masterpiece of your choosing.</p>
         </div>
+        <div className="flowervideo">
+            <h2>A little how to! Super simple and easy. Even my three year old can do it!</h2>
+        <video width="750" height="500" controls>
+            <source src={video} type="video/mp4"/>
+        </video>
+        </div>
+        </>
     )
 }
 
