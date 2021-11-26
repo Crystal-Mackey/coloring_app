ffa3087c7acb26cc301eb775a399370d8e28e294
Crystal Mackey <moonrunner575@gmail.com>
2021-11-26 11:47:57 -0500

    added name to app

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return(
         <>
         <div>
-            <h1>Welcome to my Website</h1>
+            <h1>Colorama</h1>
 
             <p>This chance to color digitally is partially inspired by my little man.</p>
             <p>Navigate to one of the other links for a chance to choose from a variety of colors to make a masterpiece of your choosing.</p>
